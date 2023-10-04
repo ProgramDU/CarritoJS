17df4f2006a7f1283e4426aea8b75613c3270408
program.DU <bernardinodaniela96@gmail.com>
2023-10-03 22:59:42 -0600

    02 finalizar carrito

--- js/app.js
+++ js/app.js
@@ -1,35 +1,74 @@
 const listaCursos = document.querySelector('#lista-cursos');
+const tabla = document.querySelector('#lista-carrito tbody')
 
 const carrito = [];
 
 function getCurso(e){
     e.preventDefault();
-    if(e.target.Classlist.contains('agregar-carrito')){
-        const item = {}
+    if(e.target.classList.contains('agregar-carrito')){
+        const item = [];
         item.id = e.target.getAttribute('data-id');
         const padre = e.target.parentElement;
         item.name = padre.querySelector('h4').innerText;
         item.price = padre.querySelector('p span').innerText;
-        item.image = padre
-                        .parentElement
-                        .querySelector('img').src;
-        item.cantiny = 1;
-        // verificar si el objeto existe en el carrito
-        if (carrito.some(item => item.id === itemCarrito.id)){
-            carrito = carrito.map(itemCarrito => {
-                if(itemCarrito.id === item.id){
-                    itemCarrito.cantiny++;
-                    return itemCarrito;
-                } else {
-                    return itemCarrito;
-                }
-            });
-        } else {
-            carrito.push(item);
-        }
-        console.log(carrito);
+        item.image = padre.parentElement.querySelector('img').src;
+        item.cantity = 1;
+        addItem(item);
+        //Llenar la tabla
+        showTable();
+       
+    }
 
+}
+function addItem(item){
+     // verificar si el objeto existe en el carrito
+     if (carrito.some(itemCarrito => item.id === itemCarrito.id)){
+          carrito.forEach(itemCarrito => {
+              if(itemCarrito.id === item.id){
+                    itemCarrito.cantity++;
+                }
+        });
+    } else {
+        carrito.push(item);
+    }
+}      
+function showTable(){
+    //Limpiar tabla
+    tabla.innerHTML='';
+    //iterar carrito para insertar
+    carrito.forEach(item => {
+        tabla.appendChild(createRow(item));
+    });
+}
+
+function createRow(item){
+    const row = document.createElement('tr');
+    let rowHtml = ``;
+    rowHtml += `<td><img src="${ item.image }" width="100px"></td>`;
+    rowHtml += `<td>${ item.name }</td>`;
+    rowHtml += `<td>${ item.price }</td>`;
+    rowHtml += `<td>${ item.cantity }</td>`;
+    const button =document.createElement('button');
+
+    button.setAttribute('data-id', item.id);
+    button.classList.add('btn');
+
+    button.innerHTML = 'X';
+    const td = document.createElement('td');
+    td.appendChild(button);
+    row.innerHTML = rowHtml;
+    row.appendChild(td);
+    return row;
+}
+
+function btnDelItem(e){
+    if(e.target.classList.contains('btn')){
+        const id = e.target.getAttribute('data-id');
+        // Eliminar carrito
+        carrito = carrito.filter(itemCarrito => itemCarrito.id !== id);
+        showTable();
     }
 }
 
 listaCursos.addEventListener('click', getCurso);
+tabla.addEventListener('click', btnDelItem);
